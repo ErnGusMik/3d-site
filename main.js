7b7fb09a4ab39166bbb48383b1606113ae41d6c5
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-07-11 16:57:28 +0300

    created a car

--- main.js
+++ main.js
@@ -1,25 +1,122 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight);
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth / aspectRatio;
+
+const camera = new THREE.OrthographicCamera(
+    cameraWidth / -2,
+    cameraWidth / 2,
+    cameraHeight / 2,
+    cameraHeight / -2,
+    0,
+    1000
+);
+
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 10, 0);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const createWheels = () => {
+    const geometry = new THREE.BoxGeometry(12, 12, 33);
+    const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
+    const wheel = new THREE.Mesh(geometry, material);
+    return wheel;
+};
 
-camera.position.z = 5;
+const createCar = () => {
+    const car = new THREE.Group();
 
-const animate = function () {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    renderer.render(scene, camera);
-}
+    const backWheel = createWheels();
+    backWheel.position.y = 6;
+    backWheel.position.x = -18;
+    car.add(backWheel);
 
-renderer.setAnimationLoop(animate);
+    const frontWheel = createWheels();
+    frontWheel.position.y = 6;
+    frontWheel.position.x = 18;
+    car.add(frontWheel);
+
+    const main = new THREE.Mesh(
+        new THREE.BoxGeometry(60, 15, 30),
+        new THREE.MeshLambertMaterial({ color: 0xa52523 })
+    );
+    main.position.y = 12;
+    car.add(main);
+
+    const carFrontTexture = getCarFrontTexture();
+
+    const carBackTexture = getCarFrontTexture();
+
+    const carRightSideTexture = getCarSideTexture();
+
+    const carLeftSideTexture = getCarSideTexture();
+
+    carLeftSideTexture.center = new THREE.Vector2(0.5, 0.5);
+    carLeftSideTexture.rotation = Math.PI;
+    carLeftSideTexture.flipY = false
+
+    const cabin = new THREE.Mesh(new THREE.BoxGeometry(33, 12, 24), [
+        new THREE.MeshLambertMaterial({ map: carFrontTexture }),
+        new THREE.MeshLambertMaterial({ map: carBackTexture }),
+        new THREE.MeshLambertMaterial({ color: 0xffffff }), // top
+        new THREE.MeshLambertMaterial({ color: 0xffffff }), // bottom
+        new THREE.MeshLambertMaterial({ map: carRightSideTexture }),
+        new THREE.MeshLambertMaterial({ map: carLeftSideTexture }),
+      ]);
+    cabin.position.x = -6;
+    cabin.position.y = 25.5;
+    car.add(cabin);
+
+    return car;
+};
+
+const getCarFrontTexture = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 64;
+    canvas.height = 32;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 64, 32);
+
+    context.fillStyle = "#666666";
+    context.fillRect(8, 8, 48, 24);
+
+    return new THREE.CanvasTexture(canvas);
+};
+
+const getCarSideTexture = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 128;
+    canvas.height = 32;
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 128, 32);
+
+    context.fillStyle = "#666666";
+    context.fillRect(10, 8, 38, 24);
+    context.fillRect(58, 8, 60, 24);
+
+    return new THREE.CanvasTexture(canvas);
+};
+
+const car = createCar();
+scene.add(car);
+
+renderer.render(scene, camera);
